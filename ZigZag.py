be77d6960fed8a9154bff847c48ac95b5850daf7
nonceanfish <nonceanfish@gmail.com>
2018-11-22 21:40:30 -0500

    1

--- ZigZag.py
+++ ZigZag.py
@@ -11,8 +11,8 @@ class Solution:
         # format the string into matrix base on numRows*colimns
         strLength = len(s)
         oneZig = numRows + numRows - 2
-        total_zig = strlength // oneZig
-        remain_element = strlength % oneZig
+        total_zig = strLength // oneZig
+        remain_element = strLength % oneZig
         columns = total_zig * (numRows - 2)
         if remain_element - numRows >= 0:
             columns = columns + 1 + (remain_element - numRows)
@@ -20,16 +20,26 @@ class Solution:
             columns = columns + 1
 
         index = 0
-        zigM[numRows][columns] = []
+        zigM = [numRows * [''] for i in range(columns)]
         for i in range(total_zig):
             zigM[i * (numRows - 1)] = s[i * oneZig:i * oneZig + numRows]
 
-        while (index < strlength):
+            for j in range(numRows - 2):
+                zigM[i * (numRows - 1) + j + 1][numRows - j - 1] = s[i * oneZig + numRows + j]
+
+        res = ""
+        for m in range(numRows):
+            for n in range(columns):
+                if zigM[n][m]: res = res + zigM[n][m]
+
+        return res
 
 
 def main():
+    input = 'PAYPALISHIRING'
+    n = 3
     slt = Solution()
-    res = slt.convert()
+    res = slt.convert(input, 3)
     print(res)
 
 
