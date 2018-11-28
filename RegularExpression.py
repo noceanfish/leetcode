c4af103fcead548c44e34b46a8010a695b5e967e
nonceanfish <nonceanfish@gmail.com>
2018-11-27 23:37:09 -0500

    1

--- RegularExpression.py
+++ RegularExpression.py
@@ -5,40 +5,48 @@ class Solution:
         :type p: str
         :rtype: bool
         """
-        if not s or not p: return False
+        if not p:
+            return not s
 
-        s_index = 0
-        p_index = 0
-        sLen = len(s)
-        pLen = len(p)
+        first_match = bool(s) and p[0] in {s[0], '.'}
+        if len(p) >= 2 and p[1] == '*':
+            return self.isMatch(s, p[2:]) or first_match and self.isMatch(s[1:], p)
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
+        # if not s or not p: return False
 
-        while s_index < sLen:
-            if p_index >= pLen: return False
-
-            if s[s_index] == p[p_index] or p[p_index] == '.':
-                s_index += 1
-                p_index += 1
-            elif p[p_index] == '*':
-                if p_index == 0:
-                    return False
-                if s[s_index] == p[p_index - 1]:
-                    s_index += 1
-                elif p[p_index - 1] == '.':
-                    s_index += 1
-                elif p_index + 1 < pLen and s[s_index] == p[p_index + 1]:
-                    s_index += 1
-                    p_index += 2
-                elif p_index + 1 < pLen and p[p_index + 1] == '.':
-                    s_index += 1
-                    p_index += 2
-                else:
-                    return False
-            elif p_index + 1 < pLen and p[p_index + 1] == '*':
-                p_index += 2
-            else:
-                return False
-
-        return True
+        # s_index = 0
+        # p_index = 0
+        # sLen = len(s)
+        # pLen = len(p)
+        #
+        # while s_index < sLen:
+        #     if p_index >= pLen: return False
+        #
+        #     if s[s_index] == p[p_index] or p[p_index] == '.':
+        #         s_index += 1
+        #         p_index += 1
+        #     elif p[p_index] == '*':
+        #         if p_index == 0:
+        #             return False
+        #         if s[s_index] == p[p_index - 1]:
+        #             s_index += 1
+        #         elif p[p_index - 1] == '.':
+        #             s_index += 1
+        #         elif p_index + 1 < pLen and s[s_index] == p[p_index + 1]:
+        #             s_index += 1
+        #             p_index += 2
+        #         elif p_index + 1 < pLen and p[p_index + 1] == '.':
+        #             s_index += 1
+        #             p_index += 2
+        #         else:
+        #             return False
+        #     elif p_index + 1 < pLen and p[p_index + 1] == '*':
+        #         p_index += 2
+        #     else:
+        #         return False
+        #
+        # return True
 
 
 def main():
